38e13d5add1d343cdfa2bc7f8611e24a6fc1ae7a
erwinwahyura <erwinwahyuramadhan@gmail.com>
2017-05-28 15:44:57 +0700

    add bcrypt, jsonwebtoken, env and users controller

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -5,10 +5,12 @@ const user_model = require('../models/user.js')
 var bcrypt = require('bcrypt');
 const saltRounds = 10;
 var salt = bcrypt.genSaltSync(saltRounds);
+var jwt = require('jsonwebtoken');
+require('dotenv').config()
 
 var methods = {}
 //cara 1, save is more faster than .create
-methods.s = function(req,res){
+methods.signUp = function(req, res){
     var hash = bcrypt.hashSync(req.body.password, salt);
     console.log('hasnyanyaaaa ',hash);
     var user = new user_model({
@@ -22,6 +24,23 @@ methods.s = function(req,res){
       if(!err) res.send('success \n'+result)
       else res.send(err.message)
     })
+}
+
+methods.signIn = function(req, res) {
+  let username = req.body.username
+  let password = req.body.password
+  user_model.find({username: username, password: password}, function(err, result) {
+    if (bcrypt.compare(req.body.password, result.password)) {
+      var token = jwt.sign({username: result.username, email: result.email}, process.env.SECRET)
+      res.send(token)
+    } else {
+      res.send('Silahkan Login terlebih dahhulu')
+    }
+  })
+}
+
+methods.logOut = function(req, res) {
+  var token = ''
 }
 //cara 2
 // methods.add_memo2 = function(req, res) {
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,24 @@
 var express = require('express')
 var router = express.Router()
 var memo_controller = require('../controllers/memo_controller')
+var user_controller = require('../controllers/user_controller')
 
 router.get('/', function(req, res) {
   res.send('now im alive')
 })
-
+//memo
 router.post('/api/memos', memo_controller.add_memo);
 router.post('/api/memos2', memo_controller.add_memo2);
 router.get('/api/memos', memo_controller.getAllMemo);
 router.delete('/api/memos/:id', memo_controller.delete_memo);
 router.put('/api/memos/:_id', memo_controller.update_memo);
 
+
+//user
+router.post('/api/signup', user_controller.signUp);
+router.post('/api/signin', user_controller.signIn);
+// router.get('/api/users', user_controller.getAllUser);
+// router.delete('/api/users/:id', user_controller.delete_user);
+// router.put('/api/users/:_id', user_controller.update_user);
 
 module.exports = router
